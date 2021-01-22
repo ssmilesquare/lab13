aac4b1f7f0923dca081603f99f957ef0f30d86da
ssmilesquare <ssmilesquare@gmail.com>
2021-01-22 16:23:13 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	T key;
+	int i, j;
+	for(i = 1; i < N; ++i){
+		key = d[i];
+		j = i-1;
+		while(j >= 0 && d[j] < key){
+			d[j+1] = d[j];
+			--j;
+		}
+		d[j+1] = key;
+		cout << "Pass " << i << ":";
+		for (int k = 0; k < N; k++){
+			cout << d[k] << " ";  
+		}  
+    	cout << endl;
+	}
 }
 
 int main(){
